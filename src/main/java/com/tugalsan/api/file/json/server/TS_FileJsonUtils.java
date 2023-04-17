1fc239f426f13fc5fbd3a349ad91b28a744680bc
tugalsan karabacak <tugalsan@gmail.com>
2023-04-17 16:15:10 +0300

    .

--- src/main/java/com/tugalsan/api/file/json/server/TS_FileJsonUtils.java
+++ src/main/java/com/tugalsan/api/file/json/server/TS_FileJsonUtils.java
@@ -12,7 +12,7 @@ import com.tugalsan.api.unsafe.client.*;
 public class TS_FileJsonUtils {
 
     public static <T> T[] toArray(Path filePath, Class<T[]> innerClassNameArray_dot_class) {
-        return TGS_UnSafe.compile(() -> {
+        return TGS_UnSafe.call(() -> {
             var mapper = new ObjectMapper().setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             return mapper.readValue(filePath.toFile(), innerClassNameArray_dot_class);
@@ -20,7 +20,7 @@ public class TS_FileJsonUtils {
     }
 
     public static <T> T[] toArray(CharSequence jsonString, Class<T[]> innerClassNameArray_dot_class) {
-        return TGS_UnSafe.compile(() -> {
+        return TGS_UnSafe.call(() -> {
             var mapper = new ObjectMapper().setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             return mapper.readValue(jsonString.toString(), innerClassNameArray_dot_class);
@@ -28,7 +28,7 @@ public class TS_FileJsonUtils {
     }
 
     public static <T> List<T> toList(Path filePath, Class<T> innerClassName_dot_class) {
-        return TGS_UnSafe.compile(() -> {
+        return TGS_UnSafe.call(() -> {
             var mapper = new ObjectMapper().setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             return mapper.readValue(filePath.toFile(), mapper.getTypeFactory().constructCollectionType(ArrayList.class, innerClassName_dot_class));
@@ -36,7 +36,7 @@ public class TS_FileJsonUtils {
     }
 
     public static <T> List<T> toList(CharSequence jsonString, Class<T> innerClassName_dot_class) {
-        return TGS_UnSafe.compile(() -> {
+        return TGS_UnSafe.call(() -> {
             var mapper = new ObjectMapper().setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             return mapper.readValue(jsonString.toString(), mapper.getTypeFactory().constructCollectionType(ArrayList.class, innerClassName_dot_class));
@@ -47,14 +47,14 @@ public class TS_FileJsonUtils {
     //OR U WILL GET SERIALIZATION EXCEPTION
     //a = objectMapper.readValue(jsonString, Aligel.class);
     public static <T> T toObject(CharSequence jsonString, Class<T> className_dot_class) {
-        return TGS_UnSafe.compile(() -> {
+        return TGS_UnSafe.call(() -> {
             return new ObjectMapper().setVisibility(PropertyAccessor.FIELD, Visibility.ANY)
                     .readValue(jsonString.toString(), className_dot_class);
         });
     }
 
     public static String toJSON(Object o, boolean pretty) {
-        return TGS_UnSafe.compile(() -> {
+        return TGS_UnSafe.call(() -> {
             return new ObjectMapper()
                     .setVisibility(PropertyAccessor.FIELD, Visibility.ANY)
                     .enable(SerializationFeature.INDENT_OUTPUT)
